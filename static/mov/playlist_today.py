8036b16903295345dc2ec5728c0c87c4c71c0f95
root <root@localhost.localdomain>
2017-09-30 10:35:59 +0800

    new file:   playlist_today.py

---
+++ static/mov/playlist_today.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+import datetime
+
+
+yes_date = (datetime.datetime.now()+datetime.timedelta(days=-1)).strftime('%Y%m%d')
+today = (datetime.datetime.now()).strftime('%Y%m%d')
+def getfilename():
+    for i,j,k in os.walk(today):
+        k.sort()
+        return k
+
+for i in range(0,24):
+    f = open(today+'/'+'play'+str(i).zfill(2)+'.m3u8','w')
+    f.write('#EXTM3U'+'\n'+'#EXT-X-VERSION:3'+'\n'+'#EXT-X-TARGETDURATION:2'+'\n'+'#EXT-X-MEDIA-SEQUENCE:0'+'\n')
+    f.close()
+    filename = getfilename()
+    for j in filename:
+        f = open(today+'/'+'play'+str(i).zfill(2)+'.m3u8','a')
+        if  j[:2] == str(i).zfill(2):
+            f.write('#EXTINF:2.000000,'+'\n')
+            f.write(j+'\n')
+    f.write('#EXT-X-ENDLIST'+'\n')
+    f.close()
